fa9d0d2102143768afe95e92081d5bc749045137
Nino Majder <nino.majder@gmail.com>
2022-04-24 17:27:58 +0200

    Add SearchForm

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,24 @@
+import { useState } from "preact/hooks";
+import {} from "preact";
+
+export default function SearchForm() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleOnInput = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <form>
+      <label htmlFor="search" class="block mr-2">
+        Search music
+      </label>
+      <input
+        id="search"
+        type="text"
+        value={searchTerm}
+        onInput={handleOnInput}
+      />
+    </form>
+  );
+}
