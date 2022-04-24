49c5de3f4c1be34d5bb38432e86584753be30fcf
Nino Majder <nino.majder@gmail.com>
2022-04-24 14:53:29 +0200

    Inline css

---
+++ inline-css.js
@@ -0,0 +1,58 @@
+const Critters = require("critters");
+const path = require("path");
+const fs = require("fs");
+const fse = require("fs-extra");
+const { walk } = require("@root/walk");
+const { minify } = require("html-minifier-terser");
+
+function removeExternalStyles(html) {
+  return html.replace(/<link rel="stylesheet" href=".*\.css"[^>]+>/g, "");
+}
+
+const optimize = async function () {
+  // inline @fonts in style
+  const c = new Critters({
+    inlineFonts: true,
+    path: "./dist",
+    publicPath: "/",
+    preload: "body",
+  });
+
+  await fse.remove("./optimized");
+  await walk("./dist", async (err, pathname, dirent) => {
+    if (err) {
+      throw err;
+    }
+    if (dirent.isDirectory() && dirent.name.startsWith(".")) {
+      return false;
+    }
+    if (!dirent.name.match(/.*\.html$/)) {
+      if (dirent.name.indexOf(".") !== -1) {
+        //copy assets to optimized folder
+        await fse.copy(pathname, pathname.replace("dist", "optimized"));
+      }
+      return;
+    }
+
+    const html = fs.readFileSync(pathname, "utf-8");
+    const res = await c.process(html);
+    const optimizedPath = pathname.replace("dist", "optimized");
+    // remove external styles, since critters doesn't actually remove the inline styles
+    const htmlNoExternalStyle = removeExternalStyles(res);
+    const minifiedHtml = await minify(htmlNoExternalStyle, {
+      removeComments: true,
+      minifyJS: true,
+      minifyCSS: {
+        level: 2,
+      },
+    });
+
+    await fse.outputFile(optimizedPath, minifiedHtml);
+    console.log("name:", dirent.name, "in", path.dirname(pathname));
+  });
+
+  await fse.remove("./dist");
+  await fs.renameSync("./optimized", "./dist");
+};
+
+optimize();
